503ed217a4e7f45e6acea3ff3fa49498f3f9fbf4
LAB24_USER ON WS28 <L24Y12W28@LabHCI.net>
2026-07-16 14:57:41 +0800

    Fixed dependencies issue

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -45,4 +45,6 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
 }

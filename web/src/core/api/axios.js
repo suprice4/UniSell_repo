800e8dc5d15e3f86d3789df6974331b191992906
suprice4 <princecapendit@gmail.com>
2026-07-18 22:00:40 +0800

    fix: use VITE_API_BASE_URL env var instead of hardcoded localhost in axios client

--- web/src/core/api/axios.js
+++ web/src/core/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL
+    ? `${import.meta.env.VITE_API_BASE_URL}/api`
+    : "http://localhost:8080/api",
 });
 
 api.interceptors.request.use((config) => {
